1aa9a8a20f71fe3acce1d627a218cdbf12908653
Ingmar Falk <if222gq@student.lnu.se>
2024-09-24 13:45:25 +0200

    feat: set pixels to plus on home route

--- main.py
+++ main.py
@@ -5,9 +5,32 @@ import uvicorn
 app     = fp.FastAPI()
 sense   = sh.SenseHat()
 
+green = (0, 255, 0)
+yellow = (255, 255, 0)
+blue = (0, 0, 255)
+red = (255, 0, 0)
+white = (255,255,255)
+nothing = (0,0,0)
+pink = (255,105, 180)
+
+W=white
+O=nothing
+
+
+plus = [
+    O, O, O, O, O, O, O, O, 
+    O, O, O, W, W, O, O, O,
+    O, O, O, W, W, O, O, O, 
+    O, W, W, W, W, W, W, O,
+    O, W, W, W, W, W, W, O,
+    O, O, O, W, W, O, O, O,
+    O, O, O, W, W, O, O, O,
+    O, O, O, O, O, O, O, O
+]
+
 @app.get("/")
 async def root():
-    return { "message": "Moin" }
+    sense.set_pixels(plus)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
