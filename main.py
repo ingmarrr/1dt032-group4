14a344244eb7c0b6463aa82050c0df1b50ff3121
Ingmar Falk <if222gq@student.lnu.se>
2024-09-24 13:35:39 +0200

    removed local environment

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import fastapi as fp
+import sense_hat as sh
+import uvicorn
 
-app = fp.FastAPI()
+app     = fp.FastAPI()
+sense   = sh.SenseHat()
 
 @app.get("/")
 async def root():
     return { "message": "Moin" }
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
